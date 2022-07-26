36b8ad122cfb816790808723ddfb90f34e16095a
Kyle Hanks <kylehanks@pm.me>
2022-07-26 16:06:17 -0700

    install choco packages

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -2,11 +2,16 @@ $ErrorActionPreference = "SilentlyContinue"
 
 $category = $env:CATEGORY
 
+Write-Output "Installing packages for category $category"
+
+$ret = ""
 try {
     $ret = iex "choco install C:\\packages\\$category.config -y"
 }
 catch {
 
 }
+
+Write-Output "$ret"
 
 exit 0
