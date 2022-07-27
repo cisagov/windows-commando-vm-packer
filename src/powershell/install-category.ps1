ce4f5a776afc19cc90255b5bb599b95850e08ef0
Kyle Hanks <kylehanks@pm.me>
2022-07-26 19:34:50 -0700

    install choco packages

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -1,17 +1,15 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 $category = $env:CATEGORY
-
 Write-Output "Installing packages for category $category"
 
-$ret = ""
-try {
-    $ret = iex "choco install C:\\packages\\$category.config -y"
-}
-catch {
+[xml]$xml = Get-Content "C:\\packages\\$category.config"
 
+$xml.SelectNodes('//packages/package') | ForEach-Object {
+    $packageID = $_.id
+    Write-Output "### current package: $packageID ####################################################"
+    try { choco upgrade $packageID -y --no-progress }
+    catch { }
 }
-
-Write-Output "$ret"
 
 exit 0
