f186eab9bf3e9888859bd2358e4e4ee9e0c66912
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-05-03 12:16:43 -0700

    update comments in extend-volume script

--- src/powershell/extend-volume.ps1
+++ src/powershell/extend-volume.ps1
@@ -3,17 +3,19 @@ Write-Output "[ ] Attempting to extend C drive volume"
 # Scan the disk to detect the current volume size
 "rescan" | diskpart
 
-# Get the C drive volume
+# Get the current C drive volume
 $originalVolumeSizeGB = $(Get-Volume -DriveLetter C).Size / 1GB
 
-# Get the C drive partition to the maximum Amazon EBS volume size available
+# Partition the C drive to the maximum Amazon EBS volume size available
 Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
 
 # Rescan the disk to detect the updated volume size
 "rescan" | diskpart
 
-# Verify the C drive volume was extended
+# Get the updated C drive volume
 $volumeSizeGB = $(Get-Volume -DriveLetter C).Size / 1GB
+
+# Verify the C drive volume was extended
 Write-Output "[ ] Verifying C drive volume was extended"
 if ($volumeSizeGB -le $originalVolumeSizeGB) {
     Write-Error "[X] Failed to extend C drive volume" -ErrorAction Stop
