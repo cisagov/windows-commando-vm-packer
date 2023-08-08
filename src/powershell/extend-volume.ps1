88ec81ecc092a87cbc39fdafbe437eca2ce3ea88
itsmostafa <mostafaxcodes@gmail.com>
2023-08-08 13:33:22 -0700

    debug: get more disk info

--- src/powershell/extend-volume.ps1
+++ src/powershell/extend-volume.ps1
@@ -1,8 +1,9 @@
 Write-Output "[ ] Attempting to extend C drive volume"
 
-# Stop-Service -Name ShellHWDetection
-# Get-Disk | Where PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "EBS Volume" -Confirm:$false
-# Start-Service -Name ShellHWDetection
+# Initialize Raw Disk and Assign it to a Drive Letter
+Stop-Service -Name ShellHWDetection
+Get-Disk | Where PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "EBS Volume" -Confirm:$false
+Start-Service -Name ShellHWDetection
 
 # Scan the disk to detect the current volume size
 "rescan" | diskpart
@@ -10,13 +11,22 @@ Write-Output "[ ] Attempting to extend C drive volume"
 # Get the current available disks
 "list disk" | diskpart
 
-Get-Disk
+$disks = Get-Disk  
+foreach ($disk in $disks)  
+{  
+    $partitions = Get-Partition -DiskNumber $disk.Number  
+    foreach ($partition in $partitions)  
+    {  
+        Write-Output "LUN Number: $($partition.Number)"  
+        Write-Output "Drive Letter: $($partition.DriveLetter)"  
+    }  
+}
 
 # Get the current C drive volume
 $originalVolumeSizeGB = $(Get-Volume -DriveLetter C).Size / 1GB
 
 # Partition the C drive to the maximum Amazon EBS volume size available
-Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax -ErrorAction "continue"
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter D).SizeMax -ErrorAction "continue"
 
 # Do not continue if the C drive volume is already extended
 if (!$?) {
