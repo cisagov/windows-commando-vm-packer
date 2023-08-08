3e47b0c5a3caf7f943f1fda426be98aec969abda
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-08-08 16:36:18 -0700

    uncomment all packages

--- src/powershell/extend-volume.ps1
+++ src/powershell/extend-volume.ps1
@@ -1,6 +1,6 @@
 Write-Output "[ ] Attempting to initialize D drive volume"
 
-# Initialize Raw Disk from EBS Volume and Assign it to Drive letter D
+# Initialize a raw disk from an EBS Volume and assign it to Drive letter D
 Stop-Service -Name ShellHWDetection
 Get-Disk | Where PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "EBS Volume" -Confirm:$false
 Start-Service -Name ShellHWDetection
