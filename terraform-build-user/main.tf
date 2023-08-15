8e578e130bb2f44b9f850f481980f3d420a0c232
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-08-15 08:57:17 -0700

    set amicreate max session to 2 hours to match gh actions

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -10,8 +10,8 @@ module "iam_user" {
   }
 
   # This image can take a while to build, so we set the max session
-  # duration to 3 hours.
-  ec2amicreate_role_max_session_duration = 3 * 60 * 60
+  # duration to 2 hours.
+  ec2amicreate_role_max_session_duration = 2 * 60 * 60
   ssm_parameters                         = ["/windows/commando/administrator/password"]
   user_name                              = "build-windows-commando-vm-packer"
 }
