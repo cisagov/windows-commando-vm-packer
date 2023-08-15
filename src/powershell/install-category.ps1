85cbaed820d6399f84419b360ec22677e0369050
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-08-15 08:38:38 -0700

    replace single to double quotes on install-category.ps1

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -3,7 +3,7 @@ Write-Output "Installing packages for category $category"
 
 [xml]$xml = Get-Content "D:\\packages\\$category.config"
 
-$xml.SelectNodes('//packages/package') | ForEach-Object {
+$xml.SelectNodes("//packages/package") | ForEach-Object {
     $packageID = $_.id
     Write-Output "### current package: $packageID ####################################################"
     choco upgrade $packageID --yes --no-progress
