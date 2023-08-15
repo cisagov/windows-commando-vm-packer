b70e6a9717fd9957796e210b95d009a6554123c9
Mostafa Abdo <mostafaxcodes@gmail.com>
2023-08-15 12:53:58 -0700

    Rewrite and format comments in initialize-volume.ps1
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- src/powershell/initialize-volume.ps1
+++ src/powershell/initialize-volume.ps1
@@ -1,11 +1,11 @@
 Write-Output "[ ] Attempting to initialize D drive volume"
 
-# Initialize a raw disk from an EBS Volume and assign it to Drive letter D
+# Initialize a raw disk from an EBS volume and assign it to drive letter D
 Stop-Service -Name ShellHWDetection
 Get-Disk | Where PartitionStyle -eq "raw" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "EBS Volume" -Confirm:$false
 Start-Service -Name ShellHWDetection
 
-# Get the D drive volume
+# Get the D drive volume size in GB
 $volumeSizeGB = $(Get-Volume -DriveLetter D).Size / 1GB
 
 Write-Output "[*] Successfully initialized D drive volume: $volumeSizeGB GB"
