db7aec91135e0cb37424646dda659513c6c23ced
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-08-17 08:00:38 -0700

    fix: driveLetter string formatting

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -1,5 +1,6 @@
 $category = $env:CATEGORY
 $driveLetter = $env:DriveLetter
+Write-Output "Set write to $driveLetter drive"
 Write-Output "Installing packages for category $category"
 
 # Set Chocolatey install directory to the assigned drive letter
@@ -7,7 +8,7 @@ Write-Output "Installing packages for category $category"
 setx ChocolateyInstall ${driveLetter}:\Chocolatey /M
 SET "ChocolateyInstall=${driveLetter}:\Chocolatey"
 
-[xml]$xml = Get-Content "$driveLetter:\\packages\\$category.config"
+[xml]$xml = Get-Content "${driveLetter}:\\packages\\$category.config"
 
 $xml.SelectNodes("//packages/package") | ForEach-Object {
     $packageID = $_.id
