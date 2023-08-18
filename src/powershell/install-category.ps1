cf13e226f07877eea78d4640572ccd6d3dee757b
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-08-18 09:00:43 -0700

    string formatting fix

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -1,14 +1,14 @@
 $category = $env:CATEGORY
 $driveLetter = $env:DriveLetter
 $packagesDir = $env:PackagesDir
-Write-Output "Installing packages for category $category in $driveLetter:\\$packagesDir"
+Write-Output "Installing packages for category ${category} in ${driveLetter}:\\${packagesDir}"
 
 # Set Chocolatey install directory to the assigned drive letter
 # This must be re-set in between Windows restarts
 setx ChocolateyInstall ${driveLetter}:\Chocolatey /M
 SET "ChocolateyInstall=${driveLetter}:\Chocolatey"
 
-[xml]$xml = Get-Content "${driveLetter}:\\$packagesDir\\$category.config"
+[xml]$xml = Get-Content "${driveLetter}:\\${packagesDir}\\${category}.config"
 
 $xml.SelectNodes("//$packagesDir/package") | ForEach-Object {
     $packageID = $_.id
