d2e72cb9082563fc8f806afa594764780519d98a
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-09-06 18:47:36 -0700

    refactor set-wallpaper.ps1

--- src/powershell/set-wallpaper.ps1
+++ src/powershell/set-wallpaper.ps1
@@ -1,31 +1,29 @@
 Write-Output "[ ] Attempting to set Commando VM wallpaper"
 
-$path = $args[0]
-$setwallpapersrc = @"
-using System.Runtime.InteropServices;
-
-public class Wallpaper
-{
-  public const int SetDesktopWallpaper = 20;
-  public const int UpdateIniFile = 0x01;
-  public const int SendWinIniChange = 0x02;
-  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-  public static void SetWallpaper(string path)
-  {
-    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
-  }
-}
-"@
-
-Add-Type -TypeDefinition $setwallpapersrc
-
 $sourceImage = "https://raw.githubusercontent.com/mandiant/commando-vm/main/Images/background.png"
 $destinationImage = "C:\background.png"
 
+# Utilize user32.dll module which allows the manipulation
+# of standard elements of the Windows user interface
+$setWallpaperSrc = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+
+     public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+         public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3);
+         }
+    }
+ }
+'@
+
+Add-Type $setWallpaperSrc
 try {
   Invoke-WebRequest $sourceImage -OutFile $destinationImage
-  [Wallpaper]::SetWallpaper("C:\background.png")
+  [Win32.Wallpaper]::SetWallpaper($destinationImage)
   Write-Output "[*] Successfully set Commando VM wallpaper"
 } catch {
   Write-Error "[*] Failed to set Commando VM wallpaper" -ErrorAction Stop
