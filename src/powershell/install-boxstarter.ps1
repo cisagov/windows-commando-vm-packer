4b0ef07a29b9de24ef2b7c94a7edab347181c92f
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-09-08 08:42:13 -0700

    add additional info to install-boxstarter.ps1

--- src/powershell/install-boxstarter.ps1
+++ src/powershell/install-boxstarter.ps1
@@ -10,8 +10,15 @@ function Set-BoxstarterConfiguration {
     $Boxstarter.AutoLogin = $true
     $Boxstarter.SuppressLogging = $true
     $global:VerbosePreference = "SilentlyContinue"
+
+    # Add Chocolatey and Mandiant VM-Packages repositories as Boxstarter sources
     Set-BoxstarterConfig -NugetSources "$desktopPath;.;https://www.myget.org/F/vm-packages/api/v2;https://chocolatey.org/api/v2"
+
+    # Set Windows Explorer options
+    # See: https://boxstarter.org/winconfig#set-windowsexploreroptions
     Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+
+    # Import Boxstarter Chocolatey module to enable Chocolatey commands
     Import-Module "${Env:ProgramData}\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1" -Force
 }
 
