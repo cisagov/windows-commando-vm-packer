af8e5e7e8a1cb6acf4b318b1fa2d72168ce6d051
Mostafa Abdo <mostafaxcodes@gmail.com>
2023-09-08 08:45:26 -0700

    modify string formatting in install-category.ps1
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- src/powershell/install-category.ps1
+++ src/powershell/install-category.ps1
@@ -4,7 +4,7 @@ Write-Output "Installing packages for category ${category} in C:\\${packagesDir}
 
 [xml]$xml = Get-Content "C:\\${packagesDir}\\${category}.config"
 
-$xml.SelectNodes("//$packagesDir/package") | ForEach-Object {
+$xml.SelectNodes("//${packagesDir}/package") | ForEach-Object {
     $packageID = $_.id
     Write-Output "### current package: $packageID ####################################################"
     Install-BoxstarterPackage -PackageName $packageID
