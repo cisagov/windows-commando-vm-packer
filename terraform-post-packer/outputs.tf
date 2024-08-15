24ca1bd6e8f3b2d883d2ffc65e2fcf1853d1bdf9
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-08-15 16:39:45 -0400

    Remove support for ARM64
    There are no Windows Server ARM64 base AMIs.

--- terraform-post-packer/outputs.tf
+++ terraform-post-packer/outputs.tf
@@ -1,7 +1,8 @@
-output "launch_permissions_arm64" {
-  value       = module.ami_launch_permission_arm64
-  description = "The cisagov/ami-launch-permission-tf-module for each ARM64 AMI to which launch permission is being granted."
-}
+# There are no Windows Server ARM64 base AMIs.
+# output "launch_permissions_arm64" {
+#   value       = module.ami_launch_permission_arm64
+#   description = "The cisagov/ami-launch-permission-tf-module for each ARM64 AMI to which launch permission is being granted."
+# }
 
 output "launch_permissions_x86_64" {
   value       = module.ami_launch_permission_x86_64
